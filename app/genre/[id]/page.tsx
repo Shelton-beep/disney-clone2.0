160b3f01ec42a6019efe6825cbe371d6ab4f17e6
Shelton Simbi <shltnsimbi@gmail.com>
2024-01-05 13:46:22 +0200

    created genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,6 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+
 type Props = {
   params: {
     id: string;
@@ -7,11 +10,17 @@ type Props = {
   };
 };
 
-function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
+async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
+
   return (
-    <div>
-      GenrePage{id} and
-      {genre}
+    <div className="max-w-7xl mx-auto">
+      {/**AI Azure Suggestion */}
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+      </div>
+
+      <MoviesCarousel title="Genre" movies={movies} isVertical />
     </div>
   );
 }
