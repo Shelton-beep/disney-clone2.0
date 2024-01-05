1be31b7725b27afafc62e4d985dfe81988d48d0e
Shelton Simbi <shltnsimbi@gmail.com>
2024-01-05 09:49:55 +0200

    Created MoviesCard and Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
 import Image from "next/image";
 
-export default function Home() {
-  return <h1>Lets make Disney</h1>;
+export default async function Home() {
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
+
+  return (
+    <main>
+      {/**CarouselBannerWrapper */}
+      <div className="flex flex-col space-y-2 xl:-mt-48">
+        <MoviesCarousel title="UpComing" movies={upcomingMovies} />
+        <MoviesCarousel title="Top Rated" movies={topRatedMovies} />
+        <MoviesCarousel title="Popular" movies={popularMovies} />
+      </div>
+    </main>
+  );
 }
